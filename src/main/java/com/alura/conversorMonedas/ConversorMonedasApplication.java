cb78de22da45d6ad9e1370306d82e2a237aa6276
Marco Antonio Villanueva Soto <marco_x1230@hotmail.com>
2024-07-16 00:10:59 -0500

    creando

---
+++ src/main/java/com/alura/conversorMonedas/ConversorMonedasApplication.java
@@ -0,0 +1,13 @@
+package com.alura.conversorMonedas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConversorMonedasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConversorMonedasApplication.class, args);
+	}
+
+}
